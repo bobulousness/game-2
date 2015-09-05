35ffd44d94b0970d84b871b6602217cc637b2db2
Josh Bumgarner <bobulousness@gmail.com>
2015-09-05 12:06:23 -0400

    save

--- flap/game.js
+++ flap/game.js
@@ -3,8 +3,11 @@
 var game = new Phaser.Game(800,600,Phaser.AUTO,'game',
   {preload:preload,create:create,update:update,render:render});
 
+var background
+
 function preload() {
   game.stage.backgroundColor = '#bbbbbb';
+  game.load.image("background","strsky.gif");
 }
 
 function create() {
