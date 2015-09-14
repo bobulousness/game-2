457ac69e3d57c91b57bd030360ea121de77243b0
Josh Bumgarner <bobulousness@gmail.com>
2015-09-14 11:50:26 -0400

    save

--- flap/game.js
+++ flap/game.js
@@ -3,14 +3,14 @@
 var game = new Phaser.Game(320,568,Phaser.AUTO,'game',
   {preload:preload,create:create,update:update,render:render});
 
-var background
+var strsky
 var rktcar
 var atnrig
 var atnwro
-
+var jumpTimer = 0
 function preload() {
   game.stage.backgroundColor = '#bbbbbb';
-  game.load.image("background","strsky.gif");
+  game.load.image("strsky","strsky.gif");
   game.load.image("rktcar","rktcar.gif");
 }
 
@@ -18,11 +18,18 @@ function create() {
 
 strsky = game.add.tileSprite(0,0,320,568,'strsky');
 game.physics.startSystem(Phaser.Physics.ARCADE);
-strsky.autoScroll(-500,0);
-game.add.sprite(20,300,'rktcar');
+strsky.autoScroll(-300,0);
+rktcar = game.add.sprite(20,300,'rktcar');
+game.physics.enable(rktcar,Phaser.Physics.ARCADE);
+rktcar.body.gravity.y = 150;;
 }
 
 function update() {
+ if(game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
+  rktcar.body.velocity.y = -100;
+  jumpTimer = game.time.now + 750;
+ }
+  
 }
 
 function render() {
