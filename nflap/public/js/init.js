715304cd6fc33ea8141143d45f79cf8a1fe7a332
Josh Bumgarner <bobulousness@gmail.com>
2015-10-24 12:04:05 -0400

    save

---
+++ nflap/public/js/init.js
@@ -0,0 +1,4 @@
+//init.js
+'use strict';
+var game = new Phaser.Game(320,568);
+var Flappy = {};
---
+++ nflap/public/js/play.js
@@ -0,0 +1,28 @@
+//State: Flappy.Play
+Flappy.Play = function () {
+  this.player = null;
+};
+// the methods of the state
+Flappy.Play.prototype = {
+
+  init: function () {
+    this.game.renderer.renderSession.roundPixels = true;
+    this.physics.startSystem(Phaser.Physics.ARCADE);
+    this.background = null;
+  },
+
+  preload: function () {
+    this.load.image("background", "assets/background.png");
+  },
+
+  create: function (){
+    this.background = this.add.tileSprite(0,0,320,568,"background")
+    this.background.autoScroll(-100,0);
+  },
+
+  update: function () {
+  }
+};
+
+game.state.add('Play',Flappy.Play);
+
